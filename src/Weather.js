283432c73aeb55f924b3d6887d465284d2ad6313
RyanLin11 <ryanlin118@gmail.com>
2022-08-26 18:37:36 -0400

    Add localstorage to store places

--- src/Weather.js
+++ src/Weather.js
@@ -5,25 +5,25 @@ import './Weather.css';
 
 const google = window.google;
 
+const defaultPlace = {
+    city: 'New York',
+    placeId: 'ChIJOwg_06VPwokRYv534QaPC8g',
+    coordinates: { latitude: 40.7127753, longitude: -74.0059728 }
+};
+
 function Weather() {
     const [temp, setTemp] = useState('');
     const [wind, setWind] = useState('');
     const [description, setDescription] = useState('');
     const [photo, setPhoto] = useState('');
     const [icon, setIcon] = useState('');
-    const [place, setPlace] = useState({
-        city: 'New York',
-        placeId: 'ChIJOwg_06VPwokRYv534QaPC8g',
-        coordinates: { latitude: 40.7127753, longitude: -74.0059728 }
-    });
+    const [place, setPlace] = useState(localStorage.getItem('place') === null? defaultPlace : JSON.parse(localStorage.getItem('place')));
 
     useEffect(() => {
         async function updateWeatherData() {
             // update weather data
             const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${place.coordinates.latitude}&lon=${place.coordinates.longitude}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
             const weatherData = await weatherResponse.json();
-            console.log(weatherData);
-            console.log(place);
             setTemp(weatherData.main.temp);
             setWind(weatherData.wind.speed);
             setDescription(weatherData.weather[0].description);
@@ -46,17 +46,17 @@ function Weather() {
             });
             setPhoto(photo);
         }
-        if (place) {
-            updateWeatherData();
-        }
+        updateWeatherData();
     }, [place]);
     
     function handlePlaceChange(place) {
-        setPlace({
+        let placeInfo = {
             city: place.name,
             placeId: place.place_id,
             coordinates: { latitude: place.geometry.location.lat(), longitude: place.geometry.location.lng()}
-        });
+        };
+        setPlace(placeInfo);
+        localStorage.setItem('place', JSON.stringify(placeInfo));
     }
 
     return (
